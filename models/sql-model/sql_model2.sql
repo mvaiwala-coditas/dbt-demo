f26ef1c574bcfc7c8ce0cd04dda7b4547132f3ec
madhav.vaiwala@coditas.com <madhav.vaiwala@coditas.com>
2023-04-11 16:57:03 +0000

    successful-sql-commit

---
+++ models/sql-model/sql_model2.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='table') }}
+
+select first_name, last_name, user_id, order_date, status from 
+RAW.JAFFLE_SHOP.CUSTOMERS as c
+inner join RAW.JAFFLE_SHOP.ORDERS as o 
+on c.ID = o.USER_ID
+
